0d6c70ef6fbbfcaba905392f3464405270693391
Nevin <nevviin@gmail.com>
2019-04-14 13:20:54 +1000

    Cheking in a Simple wcf solution which has a service project anc client project.

---
+++ SimpleWcf/SimpleWcf.Client/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleWcf.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // var simpleWcfService = new simplewcfserviceProxy();
+
+            var simpleWcfService = new Service1Client();
+            var returnValue = simpleWcfService.GetData(1);
+            
+
+        }
+    }
+}
